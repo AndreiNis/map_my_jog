9d3f7346d899ba5a8d2c451dbbe19ed85a95eb52
Andrei Nistor <andrei.nistor.p@gmail.com>
2019-07-30 18:12:56 -0400

    another change

---
+++ frontend/map_my_jog.jsx
@@ -0,0 +1,13 @@
+//React
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import Root from './components/root';
+// import configureStore from './store/store';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    // store = configureStore(preloadedState);
+    const root = document.getElementById('root');
+    ReactDOM.render(<h1>hello</h1>, root);
+});
