0e7430c43d17a54d34e52f3074ac48019edaca0c
Andrei Nistor <andrei.nistor.p@gmail.com>
2019-08-09 01:23:54 -0400

    add text to splash and moooore styling

--- frontend/components/background/background.jsx
+++ frontend/components/background/background.jsx
@@ -16,13 +16,25 @@ const Background = () => {
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     &nbsp;&nbsp;&nbsp;
-                    health and fitness community.</p>
+                    health and fitness community.
+                </p>
+                <div className="text2-container">
+                    <span className="bkg-line-1-1">_____</span>
+                    <span className="bkg-text-1-1">SYNC WITH UNDER<br/>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ARMOUR SMART SHOES</span>
+                    <span className="bkg-line-2-1">_____</span>
+                </div>
+                <p className="bkg-text-2-2">
+                    The best mobile run tracking experience,<br />
+                    backed by the world's largest digital<br />
+                    health and fitness community.
+                </p>
                 <img className="women-img" src="https://mapmy.uastatic.com/7ca8061f7a49ffa4c0a53685482b14aa.png" alt="women jogging"/>
                 <span className="stack-img">
                     <img className="sky-img" src="https://mapmy.uastatic.com/9b2217b59621acf4f24bfd26d9c2c3ca.png" alt="cloudy sky over city"/>            
                     <img className = "sneaker-add" src="https://mapmy.uastatic.com/646485d1f91b975423404dadbe0ebf5e.png" alt="sneaker advertisement" />
+                    <img className="cool-map" src="https://mapmy.uastatic.com/b721f1f31d6c01f7e2e6e3e3dcb9bc4d.png" alt="map showing location"/>
                 </span>
-                <img className="cool-map" src="https://mapmy.uastatic.com/b721f1f31d6c01f7e2e6e3e3dcb9bc4d.png" alt="map showing location"/>
             </div>
         </div>
     )
