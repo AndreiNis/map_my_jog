7d1f0dc2c2cffb0a9729afda5adb496047517c27
Andrei Nistor <andrei.nistor.p@gmail.com>
2019-08-08 21:50:56 -0400

    reorganize and fix splash page

--- frontend/components/background/background.jsx
+++ frontend/components/background/background.jsx
@@ -6,22 +6,24 @@ const Background = () => {
             {/* <span className="bar-1">___________</span> */}
             {/* <span className="bar-2">___________</span> */}
             <div className="background">
-            <span className="bkg-line">________</span>
-            <span className="bkg-text-1">MAKE EVERY MILE COUNT</span>
-            <span >
-                    <img className="women-img" src="https://mapmy.uastatic.com/7ca8061f7a49ffa4c0a53685482b14aa.png" alt="women jogging"/>
-            </span>
-            <img className="sky-img" src="https://mapmy.uastatic.com/9b2217b59621acf4f24bfd26d9c2c3ca.png" alt="cloudy sky over city"/>            
+                <span className="bkg-line-1">_____</span>
+                <span className="bkg-text-1">MAKE EVERY MILE COUNT</span>
+                <span className="bkg-line-2">_____</span>
+                <p className="bkg-text-2">
+                    The best mobile run tracking experience,<br/>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    backed by the world's largest digital<br/>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    &nbsp;&nbsp;&nbsp;
+                    health and fitness community.</p>
+                <img className="women-img" src="https://mapmy.uastatic.com/7ca8061f7a49ffa4c0a53685482b14aa.png" alt="women jogging"/>
+                <span className="stack-img">
+                    <img className="sky-img" src="https://mapmy.uastatic.com/9b2217b59621acf4f24bfd26d9c2c3ca.png" alt="cloudy sky over city"/>            
+                    <img className = "sneaker-add" src="https://mapmy.uastatic.com/646485d1f91b975423404dadbe0ebf5e.png" alt="sneaker advertisement" />
+                </span>
+                <img className="cool-map" src="https://mapmy.uastatic.com/b721f1f31d6c01f7e2e6e3e3dcb9bc4d.png" alt="map showing location"/>
             </div>
-            <span>
-                <img className = "sneaker-add" src="https://mapmy.uastatic.com/646485d1f91b975423404dadbe0ebf5e.png" alt="sneaker advertisement" />
-            </span>
-            <span className="jackie">
-                <img src="https://mapmy.uastatic.com/b721f1f31d6c01f7e2e6e3e3dcb9bc4d.png" alt="map showing location"/>
-            </span>
-            <span>
-                {/* <Link exact path="/signup" className="splash-signup">Sign Up</Link> */}
-            </span>
         </div>
     )
 }
